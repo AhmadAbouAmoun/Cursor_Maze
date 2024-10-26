dae95b8ace600993d9bdce1697cbe1a0f35b5a8b
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-10-26 18:58:08 +0300

    Initial commit

--- scripts/myTools.js
+++ scripts/myTools.js
@@ -2,19 +2,28 @@ var start = document.getElementById("start");
 var end = document.getElementById("end");
 var status = document.getElementById("status");
 var boundaries = document.querySelectorAll(".boundary");
+var winnable = true;
 start.addEventListener("mouseover", function () {
     document.getElementById("status").textContent = "Game has started";
     boundaries.forEach((boundary) => {
         boundary.classList.remove("highlighted");
+        winnable = true;
     });
 });
 end.addEventListener("mouseover", function () {
-    document.getElementById("status").textContent = "Game has ended you won";
+    if (winnable) {
+        document.getElementById("status").textContent = "Game has ended you won";
+    } else {
+        document.getElementById("status").textContent = "You already lost try again";
+    }
 });
 
 boundaries.forEach((boundary) => {
     boundary.addEventListener("mouseover", function () {
-        boundary.classList.add("highlighted");
-        document.getElementById("status").textContent = "Game has ended you lost";
+        if (winnable) {
+            winnable = false;
+            boundary.classList.add("highlighted");
+            document.getElementById("status").textContent = "Game has ended you lost";
+        }
     });
 });
